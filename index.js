e703885f131b09b93bc64828cf95e8e69eb76892
Sandrazenith <142157351+Sandrazenith@users.noreply.github.com>
2024-10-31 08:16:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const person = { name: "Alice", age: 25, city: "New York" };
+// for (let key in person) {
+//   console.log(key + ": " + person[key]);
+// }
+
+function toggleMenu() {
+  const navMenu = document.querySelector('.nav-menu');
+  navMenu.classList.toggle('active');
+}
